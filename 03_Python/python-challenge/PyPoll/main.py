8fdd92f361a4b878b8c9e6562514dc97a3d2c12e
yduong <43940252+yduong3@users.noreply.github.com>
2019-02-09 10:04:44 -0500

    Organizing folders

--- 03_Python/python-challenge/PyPoll/main.py
+++ 03_Python/python-challenge/PyPoll/main.py
@@ -4,7 +4,6 @@ import csv
 csvpath = os.path.join("..", "PyPoll", "election_data.csv")
 
 poll = {}
-
 total_votes = 0
 
 with open(csvpath, newline="") as csvfile:
@@ -13,7 +12,7 @@ with open(csvpath, newline="") as csvfile:
     csv_header = next(csvfile)
     
     for row in csvreader:
-        total_votes += 1
+        total_votes = total_votes + 1
         
         if row[2] in poll.keys():
             poll[row[2]] = poll[row[2]] + 1
@@ -28,22 +27,18 @@ for key, value in poll.items():
     num_votes.append(value)
 
 vote_percent = []
-for n in num_votes:
-    vote_percent.append(round(n/total_votes*100, 1))
+for votes in num_votes:
+    vote_percent.append(round(votes/total_votes*100, 1))
 
-clean_data = list(zip(candidates, num_votes, vote_percent))
+candidate_data = list(zip(candidates, num_votes, vote_percent))
 
 winner_list = []
 
-for name in clean_data:
+for name in candidate_data:
     if max(num_votes) == name[1]:
         winner_list.append(name[0])
 
 winner = winner_list[0]
-
-if len(winner_list) > 1:
-    for w in range(1, len(winner_list)):
-        winner = winner + ", " + winner_list[w]
 
 output_file = os.path.join("..", "PyPoll", "main.txt")
 
@@ -56,7 +51,7 @@ with open(output_file, 'w') as txtfile:
     txtfile.write("\n")
     txtfile.write("-------------------------")
     txtfile.write("\n")
-    for entry in clean_data:
+    for entry in candidate_data:
         txtfile.write(entry[0] + ": " + str(entry[2]) +'%  (' + str(entry[1]) + ')\n')
     txtfile.write("------------------------")
     txtfile.write("\n")
